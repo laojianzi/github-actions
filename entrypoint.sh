ffadfc346ab016017899741d4385227461b82863
Benno Lin <blueworrybear@gmail.com>
2020-08-01 22:58:39 +0800

    its not csh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/sh -l
 
-set report=$1
-set format=$2
-set file=$3
-set comment=$4
-set event=$(echo $GITHUB_REF | cut -d '/' -f 2)
+report=$1
+format=$2
+file=$3
+comment=$4
+event=$(echo $GITHUB_REF | cut -d '/' -f 2)
 
 echo "/bin/covergates upload --report $report --type $format $file"
 
 /bin/covergates upload --report $report --type $format $file
 
 if [[ -n $comment ]] && [[ "$event" == "pull" ]]; then
-  set pr=$(echo $GITHUB_REF | cut -d '/' -f 3)
+  pr=$(echo $GITHUB_REF | cut -d '/' -f 3)
   /bin/covergates comment --report $report --number $pr
 fi
