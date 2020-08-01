6f0ab94f920fd8ace1ee35a2a7b0e6c4f00fa86a
Benno Lin <blueworrybear@gmail.com>
2020-08-01 22:45:59 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set format=$2
 set file=$3
 set comment=$4
 set event=$(echo $GITHUB_REF | cut -d '/' -f 2)
-
+echo "/bin/covergates upload --report $report --type $format $file"
 /bin/covergates upload --report $report --type $format $file
 
 if [[ -n $comment ]] && [[ "$event" == "pull" ]]; then
