b754c3e825948a204acafb822a886982e6ac768b
Benno Lin <blueworrybear@gmail.com>
2020-08-01 22:48:46 +0800

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,12 @@ set format=$2
 set file=$3
 set comment=$4
 set event=$(echo $GITHUB_REF | cut -d '/' -f 2)
+
 echo "/bin/covergates upload --report $report --type $format $file"
+
 /bin/covergates upload --report $report --type $format $file
 
 if [[ -n $comment ]] && [[ "$event" == "pull" ]]; then
-  set pr=$(echo $GITHUB_REF | cut -d '/' -f 3))
+  set pr=$(echo $GITHUB_REF | cut -d '/' -f 3)
   /bin/covergates comment --report $report --number $pr
 fi
