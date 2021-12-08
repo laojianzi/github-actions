99e7420c589e40bf2876bed4a8cb8212027b62eb
laojianzi <laojianzi1994@gmail.com>
2021-12-08 16:52:31 +0800

    ci: update ci

--- covergates.sh
+++ covergates.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 set -e
+
 report=$1
 format=$2
 file=$3
@@ -8,10 +9,10 @@ event=$(echo $GITHUB_REF | cut -d '/' -f 2)
 
 echo "commit: $GITHUB_SHA ref: $GITHUB_HEAD_REF"
 
-echo "/bin/covergates upload --report $report --type $format $file"
-/bin/covergates upload --report $report --type $format $file
+echo "API_URL=$API_URL covergates upload --report $report --type $format $file"
+covergates upload --report $report --type $format $file
 
 if [[ -n $comment ]] && [[ "$event" == "pull" ]]; then
   pr=$(echo $GITHUB_REF | cut -d '/' -f 3)
-  /bin/covergates comment --report $report --number $pr
+  covergates comment --report $report --number $pr
 fi
